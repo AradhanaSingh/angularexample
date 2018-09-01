828ffa82051a04736d22d40b7b36d60ddc678b6a
Aradhana Singh <aradhanamailbox@gmail.com>
2018-09-01 16:12:24 -0700

    added actions, increment and decrement

--- identity_reducer.ts
+++ identity_reducer.ts
@@ -8,7 +8,24 @@ interface Reducer<T> {
 }
 
 let reducer: Reducer<number> = (state: number, action: Action) => {
-    return state;
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
 };
 
-console.log( reducer(0, null) ); // -> 0
+let incrementAction: Action = { type: 'INCREMENT' }
+let decrementAction: Action = { type: 'DECREMENT' }
+
+console.log( reducer(0, incrementAction ));
+console.log( reducer(1, incrementAction )); //
+
+
+console.log( reducer(100, decrementAction )); // -> 99
+
+
+
