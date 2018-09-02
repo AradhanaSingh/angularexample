d2eb03febbdd245a21f79673e2bb59ac075f3337
Aradhana Singh <aradhanamailbox@gmail.com>
2018-09-01 21:09:37 -0700

    added a store

--- identity_reducer.ts
+++ identity_reducer.ts
@@ -19,6 +19,24 @@ let reducer: Reducer<number> = (state: number, action: Action) => {
             return state;
     }
 };
+class Store<T> {
+    private _state: T;
+
+    constructor(
+        private reducer: Reducer<T>,
+        initialState: T
+    ) {
+        this._state = initialState;
+    }
+
+    getState(): T {
+        return this._state;
+    }
+
+    dispatch(action: Action): void {
+        this._state = this.reducer(this._state, action);
+    }
+}
 
 let incrementAction: Action = { type: 'INCREMENT' }
 let decrementAction: Action = { type: 'DECREMENT' }
@@ -29,9 +47,20 @@ console.log( reducer(1, incrementAction ));
 console.log( reducer(3, { type: 'PLUS', payload: 7}) );
 console.log( reducer(3, { type: 'PLUS', payload: 9000}) );
 console.log( reducer(3, { type: 'PLUS', payload: -2}) );
-
-
 console.log( reducer(100, decrementAction ));
+
+let store = new Store<number>(reducer, 0);
+console.log("Initial state of the store")
+console.log(store.getState()); // -> 0
+
+store.dispatch({ type: 'INCREMENT' });
+console.log(store.getState()); // -> 1
+
+store.dispatch({ type: 'INCREMENT' });
+console.log(store.getState()); // -> 2
+
+store.dispatch({ type: 'DECREMENT' });
+console.log(store.getState()); // -> 1
 
 
 
